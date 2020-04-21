637bbfa554c87c999c2a08d61605e21bb135a1ff
Viktor Bellavin <viktor_bellavin@epam.com>
2020-04-22 01:09:09 +0300

    Github Actions: publish on tag push; minor improvements in gradle scripts.

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,9 @@ val scriptUrl: String by extra
 
 allprojects {
     apply(from = "$scriptUrl/git-version.gradle.kts")
+}
 
+subprojects {
     repositories {
         mavenLocal()
         apply(from = "$scriptUrl/maven-repo.gradle.kts")
